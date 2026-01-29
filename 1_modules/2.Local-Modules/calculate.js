86c1c7623536f1211d0c11beaf54b3218c8e7d91
yashvi-param <yashvirwn123@gmail.com>
2026-01-29 11:44:43 +0530

    Add files via upload
    complete

---
+++ 1_modules/2.Local-Modules/calculate.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+// common js modules
+
+// module.exports = add;
+
+// es modules multiple file exporting
+
+export { add, subtraction };
+
+// es modules  default exporting
+
+// export default add
